5e915faaf4cc45563a341d04c5e845f2dcdd84c8
Daudi88 <david.strom88@gmail.com>
2021-12-07 09:02:50 +0100

    First commit

---
+++ __tests__/test.js
@@ -0,0 +1,17 @@
+const sum = require("../sum.js");
+
+describe("Testing with Jest", () => {
+  test("addition", () => {
+    const sumtest = 2 + 3;
+    const expectedResult = 5;
+    expect(sumtest).toEqual(expectedResult);
+  });
+
+  test.each([
+    [1, 1, 2],
+    [-1, 1, 0],
+    [3, 2, 5],
+  ])("Does %i + %i equals %i", (a, b, expectedResult) => {
+    expect(sum(a, b)).toBe(expectedResult);
+  });
+});
